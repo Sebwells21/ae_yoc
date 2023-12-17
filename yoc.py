3b6601987abe1b035aff55d3d80e0e06d622e9a5
Sebastian Wells <sebwells21@gmail.com>
2023-12-17 18:07:58 -0500

    Add files via upload

--- yoc.py
+++ yoc.py
@@ -1,32 +1,51 @@
-""" INITIAL THOUGHTS:
-Compare different production structures to
-find the optimal next step forward. yield_on_cost = production / cost
-key-value pair is {name:yield_on_cost}
-"""
+def get_valid_input(prompt, type_func=int, valid_check=lambda x: x > 0):
+    """ Get valid input from the user. """
+    while True:
+        try:
+            value = type_func(input(prompt))
+            if valid_check(value):
+                return value
+            else:
+                print("Invalid input, please enter a positive number.")
+        except ValueError:
+            print("Invalid input, please enter a number.")
 
-structures = []
+def get_structure_data(num_structures):
+    """ Get data for each structure. """
+    structures = []
+    for _ in range(num_structures):
+        name = input("Structure Name: ").strip()
+        production_income = get_valid_input("Production Income: ", int)
+        cost = get_valid_input("Cost: ", int, lambda x: x > 0)
 
-#Prompts the user and creates the list of dicts
-x = int(input("How many structures are being compared?: "))
-for _ in range(x):
-    name = input("Structure Name: ")
-    production_income = int(input("Production Income: "))
-    cost = int(input("Cost: "))
+        yield_on_cost = production_income / cost
+        structures.append((name, yield_on_cost))
+    return structures
 
-    yield_on_cost = production_income / cost
-    dict = {name: yield_on_cost}
-    structures.append(dict)
+def find_optimal_structures(structures):
+    """ Find the structure(s) with the highest yield on cost. """
+    highest_yield = 0
+    optimal_structures = []
 
-
-optimal_structure = None
-highest_yield = 0
-
-#Iterates through the list of dicts and finds the optimal structure
-for structure in structures:
-    for name, yield_on_cost in structure.items():
+    for name, yield_on_cost in structures:
         if yield_on_cost > highest_yield:
             highest_yield = yield_on_cost
-            optimal_structure = name
+            optimal_structures = [name]
+        elif yield_on_cost == highest_yield:
+            optimal_structures.append(name)
 
+    return optimal_structures, highest_yield
 
-print(f"The optimal structure is the {optimal_structure} with a yield on cost of {highest_yield}.")
+# Main program execution
+if __name__ == "__main__":
+    num_structures = get_valid_input("How many structures are being compared?: ", int, lambda x: x >= 1)
+    structure_list = get_structure_data(num_structures)
+    optimal_structures, highest_yield = find_optimal_structures(structure_list)
+
+    if optimal_structures:
+        formatted_yield = "{:.4f}".format(highest_yield)
+        optimal_structures_str = ", ".join(optimal_structures)
+        print(f"The optimal structure(s) is/are {optimal_structures_str} with a yield on cost of {formatted_yield}.")
+    else:
+        print("No valid structures were entered.")
+
