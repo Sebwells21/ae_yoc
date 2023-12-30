6516ef92e141ede63cebc62eb621bfe41c40637c
Sebastian Wells <sebwells21@gmail.com>
2023-12-29 21:55:00 -0500

    Update yoc.py

--- yoc.py
+++ yoc.py
@@ -17,20 +17,20 @@ def get_structure_data(num_structures):
 
 def find_optimal_structures(structures):
     highest_yield = 0
-    optimal_structures = ""
+    optimal_structure = ""
     for name, production_income, cost in structures:
         yield_on_cost = production_income / cost
         if yield_on_cost > highest_yield:
             highest_yield = yield_on_cost
-            optimal_structures = name
+            optimal_structure = name
         elif yield_on_cost == highest_yield:
-            optimal_structures += ", " + name
-    return optimal_structures, highest_yield
+            optimal_structure += ", " + name
+    return optimal_structure, highest_yield
 
 num_structures = get_valid_input("How many structures are being compared?: ")
-optimal_structures, highest_yield = find_optimal_structures(get_structure_data(num_structures))
-if optimal_structures:
+optimal_structure, highest_yield = find_optimal_structures(get_structure_data(num_structures))
+if optimal_structure:
     formatted_yield = "{:.4f}".format(highest_yield)
-    print(f"The optimal structure(s) is/are {optimal_structures} with a yield on cost of {formatted_yield}.")
+    print(f"The optimal structure(s) is/are {optimal_structure} with a yield on cost of {formatted_yield}.")
 else:
     print("No valid structures were entered.")
