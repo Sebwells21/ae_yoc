0c9e3f9b45b78f9a1bf128069110db7890a56411
Sebastian Wells <sebwells21@gmail.com>
2023-12-29 21:20:13 -0500

    Update yoc.py

--- yoc.py
+++ yoc.py
@@ -1,8 +1,8 @@
-def get_valid_input(prompt, type_func=int, valid_check=lambda x: x > 0):
+def get_valid_input(prompt):
     while True:
         try:
-            value = type_func(input(prompt))
-            if valid_check(value):
+            value = int(input(prompt))
+            if value > 0:
                 return value
         except ValueError:
             pass
@@ -12,14 +12,14 @@ def get_structure_data(num_structures):
     for i in range(num_structures):
         name = input("Structure Name: ").strip()
         production_income = get_valid_input("Production Income: ")
-        cost = get_valid_input("Cost: ", valid_check=lambda x: x > 0)
-        yield_on_cost = production_income / cost
-        yield name, yield_on_cost
+        cost = get_valid_input("Cost: ")
+        yield name, production_income, cost
 
 def find_optimal_structures(structures):
     highest_yield = 0
     optimal_structures = []
-    for name, yield_on_cost in structures:
+    for name, production_income, cost in structures:
+        yield_on_cost = production_income / cost
         if yield_on_cost > highest_yield:
             highest_yield = yield_on_cost
             optimal_structures = [name]
@@ -27,7 +27,7 @@ def find_optimal_structures(structures):
             optimal_structures.append(name)
     return optimal_structures, highest_yield
 
-num_structures = get_valid_input("How many structures are being compared?: ", valid_check=lambda x: x >= 1)
+num_structures = get_valid_input("How many structures are being compared?: ")
 optimal_structures, highest_yield = find_optimal_structures(get_structure_data(num_structures))
 if optimal_structures:
     formatted_yield = "{:.4f}".format(highest_yield)
